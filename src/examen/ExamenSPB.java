f53c081f216bbe48f641d03cbe69da2afdfbc8dd
sergiopb-dam <sergio.pena@iesnervion.es>
2026-03-10 11:20:49 +0100

    Examen Arrays

--- src/examen/ExamenSPB.java
+++ src/examen/ExamenSPB.java
@@ -3,8 +3,8 @@ package examen;
 public class ExamenSPB {
 
 	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
-
+		// Examen de Arrays, String y Funciones - Sergio Peña Brenes
+		
 	}
 
 }
